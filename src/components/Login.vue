1ccdc6afaf406c83b9d553a2de141b83f53c2de5
= <dobogdas@bu.edu>
2024-01-08 18:11:44 -0500

    added login spinner

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,32 @@
 <template>
-    <form @submit.prevent="handleSubmit" class="form">        
-        <slot><h2>Login</h2></slot>
+    <form @submit.prevent="handleSubmit" class="form">
+        <slot>
+            <h2>Login</h2>
+        </slot>
         <Banner :error="errorLogin" v-if="errorLogin" @closeBanner="() => {
-            errorLogin=null
+            errorLogin = null
             password = null
             email = null
-                        
+
         }" />
 
         <div class="div-form-component">
-            <label for="email" class="input-form-field">Email:</label><input type="text" id="email"
-                v-model="email" placeholder="Email" required>
+            <label for="email" class="input-form-field">Email:</label><input type="text" id="email" v-model="email"
+                placeholder="Email" required>
         </div>
         <div class="div-form-component">
             <label for="password" class="input-form-field">Password:</label><input type="password" id="password"
                 v-model="password" placeholder="Password" autocomplete="current-password" required>
         </div>
         <div class="div-form-submit">
-            <button id="login">Login</button>
-            <router-link id="create-account-link" @click="()=>{emit('closeModal')
-            sessionStore.setLanding(false)}" :to="{ name: 'Register' }"><span>Create Account</span></router-link>
+            <span v-if="submitting" >
+                <div class="spinner"></div>
+            </span>
+            <button v-else id="login">Login</button>
+            <router-link v-if="!submitting" id="create-account-link" @click="() => {
+                emit('closeModal')
+                sessionStore.setLanding(false)
+            }" :to="{ name: 'Register' }"><span>Create Account</span></router-link>
         </div>
 
     </form>
@@ -30,50 +37,50 @@ import Banner from '../components/Banner.vue'
 import { ref, defineEmits } from 'vue'
 import { useRouter } from 'vue-router'
 import login from '../composables/login'
-import {useUserStore} from "../stores/UserStore"
+import { useUserStore } from "../stores/UserStore"
 import { useSessionStore } from '@/stores/SessionStore'
 
 
-
-        
-        const router = useRouter()
-        const userStore = useUserStore()
-        const sessionStore = useSessionStore()
-        const email = ref('')
-        const password = ref('')
-        const emit = defineEmits(['closeModal'])
-        
-
-
-        const { user ,errorLogin, loadLogin } = login()
-
-
-        const handleSubmit = async () => {
-            let u = {
-                password: password.value,
-                email: email.value
-                
-            }
-            
-            await loadLogin(u)
-            
-            
-            if(user.value){
-                password.value = ''
-                email.value=''
-                emit('closeModal')
-                userStore.setUser(user.value)
-                sessionStore.setLanding(false)
-            
-            router.push('/')
-            }
-            if(errorLogin){
-
-            }
+const router = useRouter()
+const userStore = useUserStore()
+const sessionStore = useSessionStore()
+const email = ref('')
+const password = ref('')
+const emit = defineEmits(['closeModal'])
+const submitting = ref(false)
 
 
 
-        }
+const { user, errorLogin, loadLogin } = login()
+
+
+const handleSubmit = async () => {
+    submitting.value = true
+    let u = {
+        password: password.value,
+        email: email.value
+
+    }
+
+    await loadLogin(u)
+
+
+    if (user.value) {
+        password.value = ''
+        email.value = ''
+        emit('closeModal')
+        userStore.setUser(user.value)
+        sessionStore.setLanding(false)
+
+        // router.push('/')
+    }
+    if (errorLogin) {
+
+    }
+
+
+
+}
 
 
 </script >
@@ -82,6 +89,7 @@ import { useSessionStore } from '@/stores/SessionStore'
 .error-banner {
     margin-top: 10px;
 }
+
 .div-form-submit {
     display: flex;
     flex-wrap: wrap;
@@ -90,22 +98,24 @@ import { useSessionStore } from '@/stores/SessionStore'
     align-items: baseline;
 
 }
+
 #create-account-link {
 
     text-decoration: none;
 }
-span{
-    
+
+span {
+
     margin-left: 7px;
     color: white;
-    
+
 }
+
 span:hover {
     cursor: pointer;
     color: #F812F1;
 }
 
-#create-account-link{
+#create-account-link {
     margin-top: 15px;
-}
-</style>
+}</style>
